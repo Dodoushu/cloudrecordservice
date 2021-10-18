b29b12ed0f7e1b6a6a641658766e829bb9a0abb8
yangchengshu <ycs960630@163.com>
2021-10-18 12:58:11 +0800

    Initial commit

---
+++ src/main/java/com/ycs/cloudrecordservice/CloudrecordserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ycs.cloudrecordservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudrecordserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudrecordserviceApplication.class, args);
+	}
+
+}
